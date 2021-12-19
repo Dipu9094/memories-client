e77917356b7267bb0917e590c75d088d3ef0e0ad
Dipu9094 <dipusultan090@gmail.com>
2021-12-20 02:37:44 +0600

    first commit

---
+++ build/precache-manifest.036d70774dd3af41a34c6079271f5e1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41843d3a7a9d8a2d848186d76e39877",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c527a9813ee15f3a7e5",
+    "url": "/static/css/main.4e89baf1.chunk.css"
+  },
+  {
+    "revision": "36638ce157544950c533",
+    "url": "/static/js/2.2cb5e567.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2cb5e567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c527a9813ee15f3a7e5",
+    "url": "/static/js/main.d372354e.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
